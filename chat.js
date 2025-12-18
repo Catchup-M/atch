1dc89ed5e091a7658dc7ef4f05cdf32fa5823451
Catchup-M <kingmykee001@gmail.com>
2025-12-18 11:48:26 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -224,7 +224,7 @@ const ChatView = ({
       bottom: 0,
       zIndex: 2,
       background: '#f5f5f5',
-      backgroundImage: 'url(https://i.ibb.co/m5sz7vGS/images-1-5.jpg)',
+      backgroundImage: 'url(https://i.ibb.co/1GJJCQRc/telegram-jumbled-symbols-aud1j88vumctm57w-1.jpg)',
       backgroundSize: 'cover',
       backgroundPosition: 'center',
       backgroundRepeat: 'no-repeat',
